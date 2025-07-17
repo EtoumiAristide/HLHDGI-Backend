9789dbb84c9eab47a921515d56690275a306bd16
ETOUMI Aristide <aetoumi@gmail.com>
2025-07-17 06:13:54 +0000

    17-07-2025: First commit

---
+++ src/main/java/com/elpandor/hlh/HlhdgibackendApplication.java
@@ -0,0 +1,13 @@
+package com.elpandor.hlh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlhdgibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlhdgibackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elpandor/hlh/HlhdgibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elpandor.hlh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlhdgibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
