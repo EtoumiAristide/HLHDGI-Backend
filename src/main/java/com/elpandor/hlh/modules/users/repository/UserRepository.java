106767f733c4cfd68fa0fba9b0bf6666aafc2c5c
ETOUMI Aristide <aetoumi@gmail.com>
2025-07-20 22:43:29 +0000

    20-07-2025: Fin recup info facture, ajout spring sécurity

--- src/main/java/com/elpandor/hlh/modules/users/repository/UserRepository.java
+++ src/main/java/com/elpandor/hlh/modules/users/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
     @EntityGraph(attributePaths = {"roles"})
     Optional<User> findByUsername(String username);
 
